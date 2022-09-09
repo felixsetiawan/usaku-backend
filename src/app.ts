90ac915f0d3b66ca1d1dfd1f405bbc2357b53350
felixsetiawan <felixsetiawan2000@gmail.com>
2022-09-10 03:43:59 +0700

    add allowed headers

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS, allowedHeaders: ['_007', '_770', '_012'] }));
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
